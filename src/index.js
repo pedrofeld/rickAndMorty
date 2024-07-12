e43d0e2f705615544b9b59050870a03f75b66a81
pedrofeld <pedro.gael.feld@gmail.com>
2024-07-11 22:40:30 -0300

    adição dos elementos na página de listagem dos personagens

--- src/index.js
+++ src/index.js
@@ -9,3 +9,16 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(8080, () => console.log('Servidor iniciado'));
+
+const axios = require('axios');
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://rickandmortyapi.com/api');
+        console.log(response.data);
+    } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+    }
+}
+
+fetchData();
